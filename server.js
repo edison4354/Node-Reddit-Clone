f4c78e09245e77225409ab8b7f663721f07215db
Edison Li <47466335+edison4354@users.noreply.github.com>
2021-06-30 14:30:38 -0700

    Post tests implemented

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 // Require Libraries
-const express = require('express')
+const express = require('express');
 const exphbs  = require('express-handlebars');
 
 // Set db
 require('./data/reddit-db');
 
 // App Setup
-const app = express()
+const app = express();
 const port = 3000
 
-//=================================MIDDLEWARE=================================\\
+// Middleware
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
@@ -17,17 +17,13 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// Routes
- 
-app.get('/posts/new', (req, res) => {
-  res.render('posts-new')
-})
 
-//==================CONTROLLERS=======================\\
+require('./controllers/posts')(app);
 
-require('./controllers/posts.js')(app);
 
-//====================START SERVER============================\\
+module.exports = app;
+
+// Start Server
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
