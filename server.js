8f2455d2a6b8994939d85d82b556dbc3b3030840
Edison Li <47466335+edison4354@users.noreply.github.com>
2021-07-06 17:42:04 -0700

    Users can sign up

--- server.js
+++ server.js
@@ -1,30 +1,32 @@
 // Require Libraries
+require('dotenv').config();
+
 const express = require('express');
 const exphbs  = require('express-handlebars');
 
-// Set db
-require('./data/reddit-db');
+const cookieParser = require('cookie-parser');
 
 // App Setup
-const app = express();
+var app = express();
 const port = 3000
 
 // Middleware
-
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 
 require('./controllers/posts')(app);
 require('./controllers/comments.js')(app);
-
+require('./controllers/auth.js')(app);
+require('./data/reddit-db');
 
 module.exports = app;
 
 // Start Server
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Example app listening at http://localhost:${port}!`)
 })
