99ee360c301df40957ddd55bb6e39ba4d47105ed
Edison Li <47466335+edison4354@users.noreply.github.com>
2021-07-09 19:08:10 -0700

    refactored populate code

--- controllers/posts.js
+++ controllers/posts.js
@@ -48,8 +48,7 @@ module.exports = (app) => {
   // SHOW
   app.get('/posts/:id', (req, res) => {
     const currentUser = req.user;
-    // LOOK UP THE POST
-    Post.findById(req.params.id).lean().populate({ path: 'comments', populate: { path: 'author' }}).populate('author')
+    Post.findById(req.params.id).populate('comments').lean()
       .then((post) => res.render('posts-show', { post, currentUser }))
       .catch((err) => {
         console.log(err.message);
@@ -58,10 +57,9 @@ module.exports = (app) => {
 
   // SUBREDDIT
   app.get('/n/:subreddit', (req, res) => {
-    const currentUser = req.user;
-    const { subreddit } = req.params;
-    Post.find({ subreddit }).lean().populate('author')
-      .then((posts) => res.render('posts-index', { posts, currentUser }))
+    const { user } = req;
+    Post.find({ subreddit: req.params.subreddit }).lean()
+      .then((posts) => res.render('posts-index', { posts, user }))
       .catch((err) => {
         console.log(err);
       });
